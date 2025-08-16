d775dc43b533d6030dcb821986aced6c71449b78
Adi <136924439+adityamishra13@users.noreply.github.com>
2025-08-16 20:06:05 +0530

    API Updated

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
-const date = new Date();
-const year = date.getFullYear(); // Returns the four-digit year (e.g., 2025)
-const month = date.getMonth();   // Returns the month (0-11, where 0 is January)
-const day = date.getDate();      // Returns the day of the month (1-31)
-const fromdate=year+"-"+(month+1)+"-"+(day-1)
-fetch(`https://newsapi.org/v2/everything?q=india&from=${fromdate}&sortBy=publishedAt&apiKey=ade613c458dd4cc8b8d71c0633d11e53`)
+
+fetch(`https://saurav.tech/NewsAPI/top-headlines/category/health/in.json`)
   .then(response => {
     if (!response.ok) {
       throw new Error("Network response was not ok");
